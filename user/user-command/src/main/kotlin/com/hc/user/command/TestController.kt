571622c1695b2cd176fc4ad34f45c2eae54bf2c9
Hchanjune <99157556+Hchanjune@users.noreply.github.com>
2026-03-08 19:40:50 +0900

    Testing

--- user/user-command/src/main/kotlin/com/hc/user/command/TestController.kt
+++ user/user-command/src/main/kotlin/com/hc/user/command/TestController.kt
@@ -1,6 +1,5 @@
 package com.hc.user.command
 
-import io.github.hchanjune.operationresult.core.Operations
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,6 +10,9 @@ class TestController(
     @GetMapping("/test")
     fun test(): String {
         val op = testService.operations()
+        println(op.operation)
+        println(op.metrics)
+        println(op.telemetry)
         return "Hello World"
     }
 
