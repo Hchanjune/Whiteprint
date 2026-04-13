c24ea25f8ecaf41dfc2e24464730d5446a762749
Hchanjune <99157556+Hchanjune@users.noreply.github.com>
2026-04-13 20:46:17 +0900

    Obervation version up

--- platform/infra/observability/servlet/build.gradle.kts
+++ platform/infra/observability/servlet/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    api("com.github.Hchanjune.operation-manager-kit:spring-webmvc:0.5.6")
+    api("com.github.Hchanjune.operation-manager-kit:spring-webmvc:0.5.7")
 }
