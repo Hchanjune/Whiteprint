e1dbe66f947f395f6f82c1ba461924b72ff64ef6
Hchanjune <99157556+Hchanjune@users.noreply.github.com>
2026-07-01 09:52:19 +0900

    0.1.6
    AutoConfiguration Error fix

--- platform/adapter/event/inbox/src/main/kotlin/org/whiteprint/platform/adapter/event/inbox/configuration/EventInboxAutoConfiguration.kt
+++ platform/adapter/event/inbox/src/main/kotlin/org/whiteprint/platform/adapter/event/inbox/configuration/EventInboxAutoConfiguration.kt
@@ -5,11 +5,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Import
 import org.whiteprint.platform.adapter.event.inbox.configuration.jpa.JpaEventInboxConfiguration
 import org.whiteprint.platform.adapter.event.inbox.configuration.mongo.MongoEventInboxConfiguration
+import org.whiteprint.platform.adapter.event.inbox.configuration.mongo.ReactiveMongoEventInboxConfiguration
 
 @AutoConfiguration
 @Import(
     JpaEventInboxConfiguration::class,
     MongoEventInboxConfiguration::class,
+    ReactiveMongoEventInboxConfiguration::class,
 )
 @EnableConfigurationProperties(EventInboxAutoConfigurationProperties::class)
 class EventInboxAutoConfiguration
--- platform/adapter/event/outbox/src/main/kotlin/org/whiteprint/platform/adapter/event/outbox/configuration/EventOutboxAutoConfiguration.kt
+++ platform/adapter/event/outbox/src/main/kotlin/org/whiteprint/platform/adapter/event/outbox/configuration/EventOutboxAutoConfiguration.kt
@@ -5,11 +5,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Import
 import org.whiteprint.platform.adapter.event.outbox.configuration.jpa.JpaEventOutboxConfiguration
 import org.whiteprint.platform.adapter.event.outbox.configuration.mongo.MongoEventOutboxConfiguration
+import org.whiteprint.platform.adapter.event.outbox.configuration.mongo.ReactiveMongoEventOutboxConfiguration
 
 @AutoConfiguration
 @Import(
     JpaEventOutboxConfiguration::class,
     MongoEventOutboxConfiguration::class,
+    ReactiveMongoEventOutboxConfiguration::class,
 )
 @EnableConfigurationProperties(
     EventOutboxAutoConfigurationProperties::class,
