b69827ca726baaa762c085b449aff8da5bbf8839
Hchanjune <99157556+Hchanjune@users.noreply.github.com>
2026-07-02 10:42:13 +0900

    0.3.1
    OMK Updated

--- platform/infra/observability/reactive/build.gradle.kts
+++ platform/infra/observability/reactive/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    api("com.github.Hchanjune.operation-manager-kit:spring-webflux:0.8.3")
+    api("com.github.Hchanjune.operation-manager-kit:spring-webflux:0.8.4")
 }
