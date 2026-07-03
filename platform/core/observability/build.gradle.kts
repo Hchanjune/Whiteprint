ccf20f60bc745c53ace2ff46f04af10f55c3ff00
Hchanjune <99157556+Hchanjune@users.noreply.github.com>
2026-07-03 18:28:54 +0900

    0.4.4

--- platform/core/observability/build.gradle.kts
+++ platform/core/observability/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    api("com.github.Hchanjune.operation-manager-kit:core:0.9.0")
+    api("com.github.Hchanjune.operation-manager-kit:core:0.9.1")
 }
--- platform/infra/observability/servlet/build.gradle.kts
+++ platform/infra/observability/servlet/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    api("com.github.Hchanjune.operation-manager-kit:servlet:0.9.0")
+    api("com.github.Hchanjune.operation-manager-kit:servlet:0.9.1")
 }
